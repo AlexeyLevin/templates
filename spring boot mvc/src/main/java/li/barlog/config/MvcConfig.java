68bc99ad1d1daeed6972e972117f077fad3b141f
Barlog <barlog@tanelorn.li>
2015-03-11 17:17:23 +0300

    Added tests to Spring Boot MVC template

--- spring boot mvc/src/main/java/li/barlog/config/MvcConfig.java
+++ spring boot mvc/src/main/java/li/barlog/config/MvcConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class MvcConfig extends WebMvcConfigurerAdapter {
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/").setViewName("index");
+		//registry.addViewController("/").setViewName("index");
 		registry.addViewController("/index").setViewName("index");
 	}
 }
