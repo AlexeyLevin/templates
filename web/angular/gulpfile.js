3e18dbab86d9c533834f53b787efa9d724474190
Barlog <barlog@tanelorn.dev>
2015-09-04 13:34:34 +0300

    Fixed gulp building in angular web template

--- web/angular/gulpfile.js
+++ web/angular/gulpfile.js
@@ -1,47 +1,75 @@
 'use strict';
 
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var usemin = require('gulp-usemin');
-var uglify = require('gulp-uglify');
-var minifyCss = require('gulp-minify-css');
-var rimraf = require('gulp-rimraf');
-var angularTemplateCache = require('gulp-angular-templatecache');
-var runSequence = require('run-sequence');
+var gulp = require('gulp'),
+	concat = require('gulp-concat'),
+	addsrc = require("gulp-add-src"),
+	filter = require('gulp-filter'),
+	useref = require('gulp-useref'),
+	uglify = require('gulp-uglify'),
+	minifyCss = require('gulp-minify-css'),
+	minifyHtml = require('gulp-minify-html'),
+	templateCache = require('gulp-angular-templatecache'),
+	ngConstant = require('gulp-ng-constant'),
+	rev = require('gulp-rev'),
+	revReplace = require('gulp-rev-replace'),
+	run = require('run-sequence'),
+	del = require('del');
 
-gulp.task('default', function(callback) {
-	runSequence('usemin', 'templates', 'merge', 'tmp', callback);
-});
-
-gulp.task('usemin', function() {
-	return gulp.src('src/index.html')
-		.pipe(usemin({
-			assetsDir: 'src',
-			css: [minifyCss(), 'concat'],
-			js: [uglify(), 'concat']
-		}))
-		.pipe(gulp.dest('dist/'));
+gulp.task('config', function () {
+	gulp.src('src/config.json')
+		.pipe(ngConstant())
+		.pipe(gulp.dest('tmp'));
 });
 
 gulp.task('templates', function() {
 	return gulp.src('src/**/_*.html')
-		.pipe(angularTemplateCache())
+		.pipe(minifyHtml({empty: true}))
+		.pipe(templateCache())
+		.pipe(gulp.dest('tmp'));
+});
+
+gulp.task('useref', ['config', 'templates'], function() {
+	var assets = useref.assets();
+	var js = filter('**/*.js', {restore: true});
+	var css = filter('**/*.css', {restore: true});
+	var index = filter('**/index.html', {restore: true});
+
+	return gulp.src('src/index.html')
+		.pipe(assets)
+
+		.pipe(js)
+		.pipe(addsrc.append('tmp/templates.js'))
+		.pipe(addsrc.append('tmp/config.js'))
+		.pipe(concat('js/bundle.js'))
 		.pipe(uglify())
-		.pipe(gulp.dest('tmp/'));
+		.pipe(rev())
+		.pipe(js.restore)
+
+		.pipe(css)
+		.pipe(minifyCss())
+		.pipe(rev())
+		.pipe(css.restore)
+
+		.pipe(assets.restore())
+
+		.pipe(useref())
+		.pipe(revReplace())
+
+		.pipe(index)
+		.pipe(minifyHtml({empty: true}))
+		.pipe(index.restore)
+
+		.pipe(gulp.dest('dist'));
 });
 
-gulp.task('merge', function() {
-	gulp.src(['dist/js/bundle.js', 'tmp/templates.js'])
-		.pipe(concat('bundle.js'))
-		.pipe(gulp.dest('dist/js/'));
+gulp.task('tmp', function () {
+	del(['tmp']);
 });
 
-gulp.task('tmp', function (cb) {
-	return gulp.src('tmp')
-		.pipe(rimraf());
+gulp.task('default', function(c) {
+	run('useref', 'tmp', c);
 });
 
-gulp.task('clean', function (cb) {
-	return gulp.src('dist')
-		.pipe(rimraf());
+gulp.task('clean', function () {
+	del(['dist']);
 });
--- web/angular/src/app/App.js
+++ web/angular/src/app/App.js
@@ -1,11 +1,13 @@
 (function (angular) {
 	'use strict';
 
-	angular.module('templates', []); // for generated $templateCache
+	angular.module('config', []);
+	angular.module('templates', []);
 	angular.module('app.controllers', ['ui.bootstrap']);
 	angular.module('app.services', ['ngResource']);
 
-	var app = angular.module('app', ['templates', 'app.controllers', 'app.services', 'ui.router']);
+	var app = angular.module('app', ['config', 'templates',
+		'app.controllers', 'app.services', 'ui.router']);
 
 	var Router = function ($stateProvider, $urlRouterProvider) {
 		$urlRouterProvider.otherwise("/");
@@ -26,11 +28,26 @@
 	Router.$inject = ['$stateProvider', '$urlRouterProvider'];
 	app.config(Router);
 
-	var Initializer = function($state) {
+	var Initializer = function($state, $injector, $log) {
+		var production
+
+		try {
+			production = $injector.get('production');
+		} catch (e) {
+			$log.log('Constant "production" not defined');
+		}
+
+		if (production) {
+			$log.log('production: ' + production);
+			app.config(['$compileProvider', function ($compileProvider) {
+				$compileProvider.debugInfoEnabled(false);
+			}]);
+		}
+
 		angular.element(document).ready(function() {
 			$state.go('home', {});
 		})
 	};
-	Initializer.$inject = ['$state'];
+	Initializer.$inject = ['$state', '$injector', '$log'];
 	app.run(Initializer);
 })(angular);
