be16cae1176cc1100571419d2e3f2afe1e675fe1
Barlog <barlog@tanelorn.li>
2016-02-17 17:24:50 +0300

    Kotlin empty template actualized

--- kotlin/empty/src/main/kotlin/li/barlog/App.kt
+++ kotlin/empty/src/main/kotlin/li/barlog/App.kt
@@ -2,7 +2,7 @@ package li.barlog
 
 import org.slf4j.LoggerFactory
 
-fun log() = LoggerFactory.getLogger("App")
+private fun log() = LoggerFactory.getLogger("App")
 
 fun main(args: Array<String>) {
     log().info("App started")
