83395307bad6deef32a0f7362d3da2b4a3c76dac
Barlog <barlog@tanelorn.li>
2016-04-19 14:51:23 +0300

    Some changes

--- java/spring/empty/src/main/java/li/barlog/App.java
+++ java/spring/empty/src/main/java/li/barlog/App.java
@@ -2,7 +2,6 @@ package li.barlog;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
